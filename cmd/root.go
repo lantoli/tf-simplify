b399c55e9534ade378d0cbc25cbf647ed13cbf66
Leo Antoli <430982+lantoli@users.noreply.github.com>
2024-07-23 00:26:29 +0200

    hasChanges

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -25,4 +27,22 @@ const (
 
 func Run(cmd *cobra.Command, args []string) {
 	fmt.Println("Hello, World!")
+
+	changes, err := hasChanges()
+	if err != nil {
+		fmt.Println("Error running terraform plan, tf-simplify can't continue: ", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Has changes: ", changes)
+}
+
+func hasChanges() (bool, error) {
+	if err := exec.Command("terraform", "plan", "-detailed-exitcode").Run(); err != nil {
+		if err.(*exec.ExitError).ExitCode() == EC_DIFF {
+			return true, nil
+		}
+		return false, err
+	}
+	return false, nil
 }
